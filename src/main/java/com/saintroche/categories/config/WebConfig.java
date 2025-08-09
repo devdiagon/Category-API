77a59f30b2ff1d601b5b9e44e7ff49a878e62dd6
devdiagon <fredericktimo@gmail.com>
2025-08-09 10:49:59 -0500

    env for DB port

--- src/main/java/com/saintroche/categories/config/WebConfig.java
+++ src/main/java/com/saintroche/categories/config/WebConfig.java
@@ -13,7 +13,6 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
                         .allowedHeaders("*");
             }
